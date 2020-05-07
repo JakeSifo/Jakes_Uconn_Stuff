a2551772075c4509d9a49076f35455057ec0171b
Jake S <bearcoco5@gmail.com>
2020-05-07 18:33:08 -0400

    monday_03

---
+++ Classwork/Week_4/Monday/03-Stu_Traverse-That-DOM/Unsolved/script.js
@@ -0,0 +1,7 @@
+var artP = document.getElementById("articles");
+var mainP = document.getElementById("main");
+
+artP.children[0].lastElementChild.style.color = "blue";
+artP.children[0].style.fontSize = "50px";
+artP.previousElementSibling.style.background = "black";
+
